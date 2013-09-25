6d01a400fc389a91309217056b97013047f2ddf8
Wu Yibin <yibin.wu@chukou1.com>
2013-09-25 09:49:00 +0800

    added:添加登录页

--- Demo.MvcApplication/Controllers/LoginController.cs
+++ Demo.MvcApplication/Controllers/LoginController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using EasyAccess.Infrastructure.Authorization.Controllers;
 
 namespace Demo.MvcApplication.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController : BaseController
     {
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            FormsAuthentication.SignOut();
+            return RedirectToUrl("/Login/Index?ReturnUrl=%2f");
+        }
+
+        private ActionResult RedirectToUrl(string url)
+        {
+            if (Url.IsLocalUrl(url))
+            {
+                return Redirect(url);
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
