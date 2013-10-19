61e2ddbcd7f044dfbf03fe9518ed356e0e0d3dab
51b <onebin.net@gmail.com>
2013-10-19 12:08:39 +0800

    updated: 添加初始化数据

--- EasyAccess.Repository/Migrations/Configuration.cs
+++ EasyAccess.Repository/Migrations/Configuration.cs
@@ -21,44 +21,117 @@ namespace EasyAccess.Repository.Migrations
         protected override void Seed(EasyAccessContext context)
         {
 
-            var roles = new List<Role> { new Role() { Id = 1, Name = "管理员", IsEnabled = true } };
-
-            var account = new Account()
+            var roles = new Role[]
             {
-                Id = 1,
-                Name = new Name
+                new Role
                 {
+                    Id = 1, Name = "管理员", IsEnabled = true
+                },
+                new Role
+                {
+                    Id = 2, Name = "试用", IsEnabled = true
+                }
+            };
 
-                    FirstName = "Yibin",
-                    LastName = "Wu",
-                    NickName = "51b"
-                },
-                Age = 24,
-                Contact = new Contact
+            var registers = new Register[]
+            {
+                new Register
                 {
-                    Email = "onebin.net@gmail.com",
-                    Phone = "020-87654321"
-                },
-                Sex = Sex.Male,
-                IsDeleted = false,
-                Memo = "https://github.com/onebin/EasyAccess",
-                Register = new Register
-                {
+                    Id = 1,
                     LoginUser = new LoginUser
                     {
-                        UserName = "Admin",
-                        Password = "kLD+u1gdwB2ddmp6OmPZFXsQfcmsTUeyhyguDMpLsEM="
+                        UserName = "admin",
+                        Password = "kLD+u1gdwB2ddmp6OmPZFXsQfcmsTUeyhyguDMpLsEM=" //123456
                     },
                     Salt = Guid.Parse("1A0AEF62-A8ED-44A3-93C9-48E9F9774B84")
                 },
-                CreateTime = DateTime.Now,
-                Roles = roles
+                new Register
+                {
+                    Id = 2,
+                    LoginUser = new LoginUser
+                    {
+                        UserName = "guestA",
+                        Password = "UMCVJZjMfZ7KpsCGvzKfqTXQM4pS9m/cW5gMi0X7/60=" //123456
+                    },
+                    Salt = Guid.Parse("A6ADE9CA-5220-47CB-B398-C7052D99964C")
+                },
+                new Register
+                {
+                    Id = 3,
+                    LoginUser = new LoginUser
+                    {
+                        UserName = "guestB",
+                        Password = "Bus4BHAJ87LZJcfk75H0ogwd3MJ0+YXky1ZQw5egG4c=" //123456
+                    },
+                    Salt = Guid.Parse("221D0784-F3AF-43D7-B718-A88910B47859")
+                }
             };
 
-            
+            var accounts= new Account[]
+            {
+                new Account
+                {
+                    Id = 1,
+                    Name = new Name
+                    {
 
-            context.Accounts.AddOrUpdate(x => x.Id, account);
+                        FirstName = "Yibin",
+                        LastName = "Wu",
+                        NickName = "51b"
+                    },
+                    Age = 24,
+                    Contact = new Contact
+                    {
+                        Email = "onebin.net@gmail.com",
+                        Phone = "020-87654321"
+                    },
+                    Sex = Sex.Male,
+                    IsDeleted = false,
+                    Memo = "https://github.com/onebin/EasyAccess",
+                    Register = registers[0],
+                    CreateTime = DateTime.Now,
+                    Roles = new List<Role> { roles[0] }
+                },
+                new Account
+                {
+                    Id = 2,
+                    Name = new Name
+                    {
 
+                        FirstName = "GuestA",
+                        LastName = "",
+                        NickName = "GuestA"
+                    },
+                    Age = 18,
+                    Contact = new Contact {},
+                    Sex = Sex.Unknown,
+                    IsDeleted = false,
+                    Register = registers[1],
+                    CreateTime = DateTime.Now,
+                    Roles = new List<Role> { roles[1] }
+                },
+                new Account
+                {
+                    Id = 3,
+                    Name = new Name
+                    {
+
+                        FirstName = "GuestB",
+                        LastName = "",
+                        NickName = "GuestB"
+                    },
+                    Age = 20,
+                    Contact = new Contact {},
+                    Sex = Sex.Unknown,
+                    IsDeleted = false,
+                    Register = registers[2],
+                    CreateTime = DateTime.Now,
+                    Roles = new List<Role> { roles[1] }
+                }
+            };
+
+            context.Accounts.AddOrUpdate(x => x.Id, accounts);
+            context.Registers.AddOrUpdate(x => x.Id, registers);
 
         }
     }
