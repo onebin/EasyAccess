7158a73b537199c5d1a01c1573c6ed91e3093133
51b <onebin.net@gmail.com>
2013-10-25 06:01:30 +0800

    updated:添加调试模式

--- Demo.MvcApplication/Scripts/Helpers/easyuiHelper.js
+++ Demo.MvcApplication/Scripts/Helpers/easyuiHelper.js
@@ -5,11 +5,20 @@
 
 var easyuiHelper = {
     tabs: {
-        add: function (title, url) {
+        add: function (title, url, debug) {
+            var appendParam = "debug=0";
+            if (debug) {
+                appendParam = "debug=1";
+            }
+            if (url.indexOf("?") > 0) {
+                url += "&" + appendParam;
+            } else {
+                url += "?" + appendParam;
+            }
             if ($('#container-tabs').tabs('exists', title)) {
                 $('#container-tabs').tabs('select', title);
             } else {
-                if (global.isDebugMode()) {
+                if (debug) {
                     var content = '<iframe scrolling="auto" frameborder="0"  src="' + url + '" style="width:100%;height:100%;"></iframe>';
                     $('#container-tabs').tabs('add', {
                         title: title,
--- Demo.MvcApplication/Scripts/Helpers/global.js
+++ Demo.MvcApplication/Scripts/Helpers/global.js
@@ -1,7 +1,2 @@
 ﻿var global = {
-    //若返回true，请将调试页面的母板页设为"_SubPage4Debug.cshtml"
-    isDebugMode: function() {
-        //return localStorage.getItem("DebugMode") == "true";
-        return false;
-    }
 };
