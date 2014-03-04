730d0f19d36ef93ccc2145f92f37fb01ed118f09
yibin.wu <yibin.wu@chukou1.com>
2014-03-04 10:58:24 +0800

    updated: 修改spring配置

--- EasyAccess.UnitTest/TestInfrastructure/TestCustomTimestamp/TestCustomTimestamp.cs
+++ EasyAccess.UnitTest/TestInfrastructure/TestCustomTimestamp/TestCustomTimestamp.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using EasyAccess.Model.EDMs;
+using EasyAccess.UnitTest.Bootstrap;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EasyAccess.UnitTest.TestInfrastructure.TestCustomTimestamp
 {
     [TestClass]
-    public class TestCustomTimestamp
+    public class TestCustomTimestamp: SpringTestBase
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestUpdateMode_Equal()
         {
+            var account = Account.FindById(1);
+            account.Age = 25;
+            Account.Update(account);
+            EasyAccessUnitOfWork.Commit();
         }
     }
 }
