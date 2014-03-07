59ccb53c36d7b972ce0b67af87b1a56433ec5510
yibin.wu <yibin.wu@chukou1.com>
2014-03-07 10:29:02 +0800

    update

--- EasyAccess.Infrastructure/Util/CustomTimestamp/CustomTimestampUpdateMode.cs
+++ EasyAccess.Infrastructure/Util/CustomTimestamp/CustomTimestampUpdateMode.cs
@@ -2,8 +2,19 @@
 {
     public enum CustomTimestampUpdateMode
     {
+        /// <summary>
+        /// 当实体数据的时间戳的原始值 等于 数据库中对应数据的时间戳时,执行更新操作
+        /// </summary>
         Equal,
 
-        GreaterThan
+        /// <summary>
+        ///  当实体数据的时间戳的当前值 大于 数据库中对应数据的时间戳时,执行更新操作
+        /// </summary>
+        GreaterThan,
+
+        /// <summary>
+        /// 当实体数据的时间戳的当前值 大于 或其原始值 等于 数据库中对应数据的时间戳时,执行更新操作
+        /// </summary>
+        GreaterThanOrEqual
     }
 }
--- EasyAccess.Infrastructure/Util/CustomTimestamp/Dialect/MsSql.cs
+++ EasyAccess.Infrastructure/Util/CustomTimestamp/Dialect/MsSql.cs
@@ -14,22 +14,29 @@ namespace EasyAccess.Infrastructure.Util.CustomTimestamp.Dialect
             {
                 var sets = columnNameAndValues.Select(x => "[" + x.Key + "] = " + "'" + x.Value + "'");
                 return "UPDATE [" + CustomTimestampCache.TableName + "] SET " + string.Join(", ", sets)
-                       + " WHERE [Id] = '" + DbEntityEntry.Property("Id").CurrentValue + "' " + GetTimestampCondition() + ";";
+                       + " WHERE [Id] = '" + DbEntityEntry.Property("Id").CurrentValue + "' AND " + GetTimestampCondition() + ";";
             }
             return string.Empty;
         }
 
         private string GetTimestampCondition()
         {
-            var condition = "AND [" + CustomTimestampCache.TimestampColumnName + "]";
+            var condition = "[" + CustomTimestampCache.TimestampColumnName + "]";
 
             switch (CustomTimestampCache.UpdateMode)
             {
+                case CustomTimestampUpdateMode.Equal:
+                    condition += " = '" + DbEntityEntry.Property(CustomTimestampCache.TimestampPropertyName).OriginalValue + "'";
+                    break;
                 case CustomTimestampUpdateMode.GreaterThan:
                     condition += " < '" + DbEntityEntry.Property(CustomTimestampCache.TimestampPropertyName).CurrentValue + "'";
                     break;
+                case CustomTimestampUpdateMode.GreaterThanOrEqual:
+                    condition = "(" + condition + " < '" + DbEntityEntry.Property(CustomTimestampCache.TimestampPropertyName).CurrentValue + "' OR "
+                        + condition + " = '" + DbEntityEntry.Property(CustomTimestampCache.TimestampPropertyName).OriginalValue + "'";
+                    break;
                 default:
-                    condition += " = '" + DbEntityEntry.Property(CustomTimestampCache.TimestampPropertyName).OriginalValue +"'";
+                    condition = "1 = 1";
                     break;
             }
             return condition;
