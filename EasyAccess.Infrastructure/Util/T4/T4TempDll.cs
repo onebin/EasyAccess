a83f9f03792728efc999011f143bd8ac5db411f3
yibin.wu <yibin.wu@chukou1.com>
2014-03-18 10:26:44 +0800

    updated

---
+++ EasyAccess.Infrastructure/Util/T4/T4TempDll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace EasyAccess.Infrastructure.Util.T4
+{
+    public class T4TempDll
+    {
+
+        public string DllFullName { get; private set; }
+        public string TempDllFullName { get; private set; }
+
+        public T4TempDll(string solutionPath, string modelMoudle)
+        {
+            DllFullName = Path.Combine(solutionPath, modelMoudle + @"\bin\Debug\" + modelMoudle + ".dll");
+            var tempDllPath = solutionPath + "\\packages\\T4TempDll\\";
+            if (File.Exists(DllFullName))
+            {
+                if (!Directory.Exists(tempDllPath))
+                {
+                    Directory.CreateDirectory(tempDllPath);
+                }
+                else
+                {
+                    foreach (var tempDll in new DirectoryInfo(tempDllPath).GetFiles())
+                    {
+                        try
+                        {
+                            File.Delete(tempDll.FullName);
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                }
+            }
+
+            TempDllFullName = tempDllPath + modelMoudle + "." + DateTime.Now.ToString("ddhhmmss") + ".dll";
+            File.Copy(DllFullName, TempDllFullName);
+        }
+
+
+
+    }
+}
