856bc42bc8d77b61b2b4e92b32e2738abe18451c
yibin.wu <yibin.wu@chukou1.com>
2014-04-02 15:23:49 +0800

    updated: 测试证明工作单元本身充当事务

---
+++ EasyAccess.UnitTest/TestRepository/TestUnitOfWork/EasyAccessUnitOfWorkTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using EasyAccess.Model.EDMs;
+using EasyAccess.UnitTest.Bootstrap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyAccess.UnitTest.TestUnitOfWork
+{
+    [TestClass]
+    public class EasyAccessUnitOfWorkTest : SpringTestBase
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DbUpdateConcurrencyException))]
+        public void TestTrans()
+        {
+            Role.Update(new Role()
+            {
+                Id = 1,
+                Name = "管理员",
+                Memo = "Test",
+            }); 
+            Role.Update(new Role()
+            {
+                Id = 3,
+                Name = "试用",
+                Memo = "Test",
+            }); 
+            Role.Update(new Role()
+            {
+                Id = 2,
+                Name = "试用",
+                Memo = "Test",
+            });
+            EasyAccessUnitOfWork.Commit();
+            Assert.Fail("期望抛出DbUpdateConcurrencyException，但实际没有！");
+        }
+    }
+}
